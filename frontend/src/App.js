7e90efd66ad8a609e711bbd568cdff2663e145c7
Micaelly Cristine <96353855+Micaelly2222@users.noreply.github.com>
2023-08-02 17:35:18 -0400

    primeiro commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,23 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import MonacoEditor from 'react-monaco-editor';
 import './App.css';
+import API_BASE_URL from './config'; 
 
-// Definindo o componente App
 const App = () => {
-  // Armazenar o código HTML inserido no editor
+  // armazenar o código HTML inserido no editor
   const [htmlCode, setHtmlCode] = useState('');
 
-  // Armazenar a contagem de tags após a pesquisa
+  // armazenar a contagem de tags após a pesquisa
   const [tagsCount, setTagsCount] = useState(null);
 
-  // Função para enviar o código HTML para o backend e realizar a pesquisa de tags
+  // função para enviar o código HTML para o backend e realizar a pesquisa de tags
   const handleSearchTags = async () => {
     try {
       // Faz a chamada GET para a rota /search_tags do backend
-      // Envia o código HTML como parâmetro da requisição
-      const response = await axios.get(`/search_tags?html_code=${encodeURIComponent(htmlCode)}`);
+      // Utiliza o URL base da API 
+      const response = await axios.get(`${API_BASE_URL}/search_tags?html_code=${encodeURIComponent(htmlCode)}`);
 
-      // Atualiza o estado com a contagem de tags obtida do backend
+      // atualiza o estado com a contagem de tags obtida do backend
       setTagsCount(response.data.tags_count);
     } catch (error) {
       // Exibe mensagem de erro no console em caso de falha na requisição
@@ -31,7 +31,7 @@ const App = () => {
     try {
       // Faz a chamada POST para a rota /upload_html do backend
       // Envia o código HTML como parâmetro da requisição
-      const response = await axios.post('/upload_html', { html_code: htmlCode });
+      const response = await axios.post(`${API_BASE_URL}/upload_html`, { html_code: htmlCode });
 
       // Exibe a mensagem de sucesso no console em caso de sucesso na requisição
       console.log(response.data.message);
@@ -72,5 +72,4 @@ const App = () => {
   );
 };
 
-// Exporta o componente App para ser usado em outros arquivos
 export default App;
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-const backendBaseUrl = "http://localhost:8000"; 
+const API_BASE_URL = 'http://localhost:8000'; 
 
-export { backendBaseUrl };
+export default API_BASE_URL;
