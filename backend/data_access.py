6c33803a1363e9f0adc1fe68e399803a8f366d1b
Micaelly Cristine <96353855+Micaelly2222@users.noreply.github.com>
2023-08-03 18:19:13 -0400

    primeiro commit

--- backend/data_access.py
+++ backend/data_access.py
@@ -54,6 +54,6 @@ def insert_page_data(page_name, tags_count):
 def get_page_info(session: Session, page_name: str):
     with get_db() as session:
         # Obtendo as informações da página e tags usando join
-        page_info = session.query(Tag.tag, CountTag.count).join(Page).join(CountTag).filter(Page.name == page_name).all()
-
-    return page_info
+        page_info = session.query(Tag.tag, CountTag.count).join(Page).join(CountTag).filter(
+            Page.name == page_name).all()
+        return page_info
